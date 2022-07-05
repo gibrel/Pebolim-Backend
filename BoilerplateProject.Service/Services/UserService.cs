964302c5e74ff22bd5dcbe1663ccb78dc30a5d26
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-05 18:25:09 -0300

    created user services and validators

---
+++ BoilerplateProject.Service/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pebolim.Domain.Entities;
+using Pebolim.Domain.Interfaces;
+
+namespace Pebolim.Service.Services
+{
+    public class UserService : BaseService<User>, IUserService
+    {
+        public UserService(
+            IUserRepository userRepository,
+            IMapper mapper
+        ) : base(userRepository, mapper)
+        {
+        }
+    }
+}
---
+++ BoilerplateProject.Service/Validators/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Pebolim.Domain.Entities;
+
+namespace Pebolim.Service.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Username)
+                .NotEmpty().WithMessage("Please fill in a username.")
+                .NotNull().WithMessage("Please fill in a username.");
+        }
+    }
+}
