f817836c1d5ddfba2f1810c6f81f22a1ac6428e3
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-05 18:26:13 -0300

    created user map and repository

---
+++ BoilerplateProject.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,74 @@
+﻿using Pebolim.Data.Context;
+using Pebolim.Domain.Entities;
+using Pebolim.Domain.Interfaces;
+
+namespace Pebolim.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly MySQLContext _mySQLContext;
+
+        public BaseRepository(MySQLContext mySQLContext)
+        {
+            _mySQLContext = mySQLContext;
+        }
+
+        public virtual async Task<bool> Insert(TEntity obj)
+        {
+            try
+            {
+                _mySQLContext.Set<TEntity>().Add(obj);
+                _mySQLContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual async Task<IList<TEntity>> Select()
+        {
+            var entities = _mySQLContext.Set<TEntity>().ToList();
+            return entities;
+        }
+
+        public virtual async Task<TEntity?> Select(int id)
+            => _mySQLContext.Set<TEntity>().SingleOrDefault(x => x.Id == id);
+
+        public virtual async Task<bool> Update(TEntity obj)
+        {
+            try
+            {
+                _mySQLContext.Entry(obj).State =
+                    Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _mySQLContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual async Task<bool> Delete(int id)
+        {
+            try
+            {
+                _mySQLContext.Set<TEntity>().Remove(await Select(id));
+                _mySQLContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BoilerplateProject.Data/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pebolim.Data.Context;
+using Pebolim.Domain.Entities;
+using Pebolim.Domain.Interfaces;
+
+namespace Pebolim.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(MySQLContext mySQLContext) : base(mySQLContext)
+        {
+
+        }
+    }
+}
