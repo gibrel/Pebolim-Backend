20d8bb9f5474379a999638aeb9bd28b7f9bad739
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-06 11:59:01 -0300

    fixed project references and added xml docs to swagger

--- Pebolim.API/Program.cs
+++ Pebolim.API/Program.cs
@@ -8,6 +8,7 @@ using Pebolim.Data.Repositories;
 using Pebolim.Domain.Entities;
 using Pebolim.Domain.Interfaces;
 using Pebolim.Service.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,13 @@ ConfigureServices(builder.Services);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
