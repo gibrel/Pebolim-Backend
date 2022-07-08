375275e05bb029c5f130212743312ea1eaa07a88
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-08 18:29:49 -0300

    added user constructor for non null initialization

--- Pebolim.Domain/Entities/User.cs
+++ Pebolim.Domain/Entities/User.cs
@@ -10,5 +10,12 @@ namespace Pebolim.Domain.Entities
         public string PasswordHash { get; set; }
         [Required]
         public string Salt { get; set; }
+
+        public User(string username, string passwordHash, string salt)
+        {
+            Username = username;
+            PasswordHash = passwordHash;
+            Salt = salt;
+        }
     }
 }
