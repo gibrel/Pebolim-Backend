3d1e150f4522322e83c4b13605db6a8885278b1c
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-12 09:02:37 -0300

    fixed base service interface fot nullable type

--- Pebolim.Domain/Interfaces/IBaseService.cs
+++ Pebolim.Domain/Interfaces/IBaseService.cs
@@ -5,13 +5,13 @@ namespace Pebolim.Domain.Interfaces
 {
     public interface IBaseService<TEntity> where TEntity : BaseEntity
     {
-        public abstract Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+        public abstract Task<TOutputModel?> Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
             where TValidator : AbstractValidator<TEntity>
             where TInputModel : class
             where TOutputModel : class;
         public abstract Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
-        public abstract Task<TOutputModel> GetById<TOutputModel>(int id) where TOutputModel : class;
-        public abstract Task<TOutputModel> Update<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+        public abstract Task<TOutputModel?> GetById<TOutputModel>(int id) where TOutputModel : class;
+        public abstract Task<TOutputModel?> Update<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
             where TValidator : AbstractValidator<TEntity>
             where TInputModel : class
             where TOutputModel : class;
