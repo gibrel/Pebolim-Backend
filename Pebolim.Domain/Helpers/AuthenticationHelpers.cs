f92d13fa87c32ce1ff87157a9d2b645fff84746a
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-19 18:20:12 -0300

    created authentication helper

---
+++ Pebolim.Domain/Helpers/AuthenticationHelpers.cs
@@ -0,0 +1,38 @@
+﻿using Pebolim.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pebolim.Domain.Helpers
+{
+    public static class AuthenticationHelpers
+    {
+        public static void ProvideSaltAndHash(this User user)
+        {
+            var salt = GenerateSalt();
+            user.Salt = Convert.ToBase64String(salt);
+            user.PasswordHash = ComputeHash(user.PasswordHash, user.Salt);
+        }
+
+        private static byte[] GenerateSalt()
+        {
+            var rng = RandomNumberGenerator.Create();
+            var salt = new byte[64];
+            rng.GetBytes(salt);
+            return salt;
+        }
+
+        public static string ComputeHash(string password, string saltString)
+        {
+            var salt = Convert.FromBase64String(saltString);
+
+            using var hashGenerator = new Rfc2898DeriveBytes(password, salt);
+            hashGenerator.IterationCount = 10101;
+            var bytes = hashGenerator.GetBytes(64);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
