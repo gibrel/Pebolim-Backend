d2564d2d6b2f868607e7f2b218119e6cb851e287
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-19 18:18:42 -0300

    created authentication repository

---
+++ Pebolim.Data/Repositories/AuthenticationRepository.cs
@@ -0,0 +1,24 @@
+﻿using Pebolim.Data.Context;
+using Pebolim.Domain.Entities;
+using Pebolim.Domain.Interfaces;
+
+namespace Pebolim.Data.Repositories
+{
+    public class AuthenticationRepository : BaseRepository<User>, IAuthenticationRepository
+    {
+        public AuthenticationRepository(PebolimDbContext pebolimDbContext) : base(pebolimDbContext)
+        {
+
+        }
+
+        public async Task<bool> ExistsUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> SelectByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
