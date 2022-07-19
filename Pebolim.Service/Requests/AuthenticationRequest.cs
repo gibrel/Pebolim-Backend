13b2a7a16f8203d45a64f8ae5c175fdaf07ee60c
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-19 18:20:54 -0300

    created authentication request and response

---
+++ Pebolim.Service/Requests/AuthenticationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Pebolim.Service.Requests
+{
+    public  class AuthenticationRequest
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
