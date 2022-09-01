d5b3bc1e84f54854b6a754e081fba339d757ec22
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:16:47 -0300

    added profile and team mapping

---
+++ Pebolim.Data/Mapping/ProfileMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pebolim.Domain.Entities;
+
+namespace Pebolim.Data.Mapping
+{
+    public class ProfileMap : IEntityTypeConfiguration<UserProfile>
+    {
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            builder.ToTable("UserProfile");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasConversion(p => p.ToString(), p => p)
+                .IsRequired()
+                .HasColumnName("ProfileName")
+                .HasColumnType("varchar(100)");
+
+            builder.HasOne(p => p.User)
+                .WithMany(u => u.Profiles)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
---
+++ Pebolim.Data/Mapping/TeamMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pebolim.Domain.Entities;
+
+namespace Pebolim.Data.Mapping
+{
+    public class TeamMap : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.ToTable("Team");
+
+            builder.HasKey(t => t.Id);
+
+            builder.HasOne(t => t.Profile)
+                .WithOne(p => p.Team)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
