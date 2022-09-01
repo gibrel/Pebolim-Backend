54abcce9abc0fcd53b5df2fee98259e2e3331325
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:22:58 -0300

    updated Program configs

--- Pebolim.API/Program.cs
+++ Pebolim.API/Program.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Pebolim.API.Configurations;
 using Pebolim.Data.Configurations;
@@ -9,6 +11,7 @@ using Pebolim.Domain.Entities;
 using Pebolim.Domain.Interfaces;
 using Pebolim.Service.Configurations;
 using Pebolim.Service.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +19,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 ConfigureServices(builder.Services);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson( o =>
+{
+    o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,12 +38,13 @@ if (app.Environment.IsDevelopment())
 }
 
 using (var scope = app.Services.CreateScope())
-using (var context = scope.ServiceProvider.GetService<PebolimDbContext>())
+using (var context = scope.ServiceProvider.GetService<DatabaseContext>())
     context?.Database.Migrate();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
@@ -48,22 +55,42 @@ void ConfigureServices(IServiceCollection services)
     services.Configure<WebApiConfigurations>(
         builder.Configuration.GetSection("Settings"));
 
-    services.Configure<PebolimConfiguration>(
+    services.Configure<DatabaseConfiguration>(
         builder.Configuration.GetSection("ConnectionStrings"));
 
     services.AddSingleton(new MapperConfiguration(config =>
     {
+        config.AddProfile<ProfileMapProfile>();
+        config.AddProfile<TeamMapProfile>();
         config.AddProfile<UserMapProfile>();
     }).CreateMapper());
 
-    services.AddDbContext<PebolimDbContext>();
+    services.AddDbContext<DatabaseContext>();
 
     builder.Services.AddScoped<IBaseRepository<User>, BaseRepository<User>>();
     builder.Services.AddScoped<IBaseService<User>, BaseService<User>>();
+    builder.Services.AddScoped<IBaseRepository<UserProfile>, BaseRepository<UserProfile>>();
+    builder.Services.AddScoped<IBaseService<UserProfile>, BaseService<UserProfile>>();
+    builder.Services.AddScoped<IBaseRepository<Team>, BaseRepository<Team>>();
+    builder.Services.AddScoped<IBaseService<Team>, BaseService<Team>>();
     builder.Services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
     builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<IProfileRegisterRepository, ProfileRegisterRepository>();
+    builder.Services.AddScoped<IProfileRegisterService, ProfileRegisterService>();
+    builder.Services.AddScoped<ITeamManagementRepository, TeamManagementRepository>();
+    builder.Services.AddScoped<ITeamManagementService, TeamManagementService>();
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer( o =>
+    {
+        var settings = new WebApiConfigurations();
+        o.TokenValidationParameters = new TokenValidationParameters()
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(settings.BearerKey ?? "alternative-bearer-key")),
+            ValidateIssuerSigningKey = true,
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+    });
 
     services.AddMvc(option => option.EnableEndpointRouting = false)
         .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
