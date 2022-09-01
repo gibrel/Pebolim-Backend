55e9bedad4813ad155530cf899ac15cbd415e21b
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:21:56 -0300

    added profile register and team management services

---
+++ Pebolim.Service/Services/ProfileRegisterService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Pebolim.Domain.Entities;
+using Pebolim.Domain.Interfaces;
+
+namespace Pebolim.Service.Services
+{
+    public class ProfileRegisterService : BaseService<UserProfile>, IProfileRegisterService
+    {
+        public ProfileRegisterService(
+            IProfileRegisterRepository profileRegisterRepository,
+            IMapper mapper
+        ) : base(profileRegisterRepository, mapper)
+        {
+        }
+
+        public Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel newProfile, int userId)
+            where TInputModel : class
+            where TOutputModel : class
+            where TValidator : AbstractValidator<UserProfile>
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int profileId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TOutputModel> Get<TOutputModel>(int profileId, int userId) where TOutputModel : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TOutputModel>> GetAll<TOutputModel>(int userId) where TOutputModel : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TOutputModel> Update<TInputModel, TOutputModel, TValidator>(TInputModel profileToUpdate, int userId)
+            where TInputModel : class
+            where TOutputModel : class
+            where TValidator : AbstractValidator<UserProfile>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
