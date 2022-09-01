93ca87ae8b0c8204da780b695fa4962eda126ef3
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:21:18 -0300

    updated user validator

--- Pebolim.Service/Validators/UserValidator.cs
+++ Pebolim.Service/Validators/UserValidator.cs
@@ -10,6 +10,14 @@ namespace Pebolim.Service.Validators
             RuleFor(u => u.Username)
                 .NotEmpty().WithMessage("Please fill in a username.")
                 .NotNull().WithMessage("Please fill in a username.");
+
+            RuleFor(u => u.PasswordHash)
+                .NotEmpty().WithMessage("Please inform a valid password.")
+                .NotNull().WithMessage("Please inform a valid password.");
+
+            RuleFor(u => u.Salt)
+                .NotEmpty().WithMessage("Error creating password salt.")
+                .NotNull().WithMessage("Error creating password salt.");
         }
     }
 }
