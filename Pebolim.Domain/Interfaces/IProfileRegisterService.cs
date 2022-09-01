23ba90538bfa8547c5b5731d6821c4d182e8c667
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:19:20 -0300

    adder profile register and team management service and repository interfaces

---
+++ Pebolim.Domain/Interfaces/IProfileRegisterService.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Pebolim.Domain.Entities;
+
+namespace Pebolim.Domain.Interfaces
+{
+    public interface IProfileRegisterService : IBaseService<UserProfile>
+    {
+        public Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel newProfile, int userId)
+            where TValidator : AbstractValidator<UserProfile>
+            where TInputModel : class
+            where TOutputModel : class;
+        public Task<List<TOutputModel>> GetAll<TOutputModel>(int userId)
+            where TOutputModel : class;
+        public Task<TOutputModel> Get<TOutputModel>(int profileId, int userId)
+            where TOutputModel : class;
+        public Task<TOutputModel> Update<TInputModel, TOutputModel, TValidator>(TInputModel profileToUpdate, int userId)
+            where TValidator : AbstractValidator<UserProfile>
+            where TInputModel : class
+            where TOutputModel : class;
+        public Task<bool> Delete(int profileId, int userId);
+    }
+}
