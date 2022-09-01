55660b56bb5be0ad5fa0f029b100fa88dcdeb731
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:18:06 -0300

    added team and userprofile entities

---
+++ Pebolim.Domain/Entities/Team.cs
@@ -0,0 +1,27 @@
+﻿using Pebolim.Domain.Enumerations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pebolim.Domain.Entities
+{
+    public class Team : BaseEntity
+    {
+        [Required]
+        public string TeamName { get; set; }
+        [Required]
+        public string Base64Flag { get; set; }
+        [Required]
+        public string PrimaryColour { get; set; }
+        [Required]
+        public string SecondaryColour { get; set; }
+        public Formation Formation { get; set; } = Formation.Formation_3_4_3;
+        public UserProfile? Profile { get; set; }
+
+        public Team(string teamName, string base64Flag, string primaryColour, string secondaryColour)
+        {
+            TeamName = teamName;
+            Base64Flag = base64Flag;
+            PrimaryColour = primaryColour;
+            SecondaryColour = secondaryColour;
+        }
+    }
+}
---
+++ Pebolim.Domain/Entities/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pebolim.Domain.Entities
+{
+    public class UserProfile : BaseEntity
+    {
+        [Required]
+        public User User { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public Team? Team { get; set; }
+
+
+        public UserProfile(User user, string name, Team? team = null)
+        {
+            User = user;
+            Name = name;
+            Team = team;
+        }
+    }
+}
