cd657a172fac0098a7573a19bd2c186b7bcbce87
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-09-01 17:18:33 -0300

    added database and formation enumerations

---
+++ Pebolim.Domain/Enumerations/DatabaseTypes.cs
@@ -0,0 +1,14 @@
+﻿using Pebolim.Domain.Helpers;
+
+namespace Pebolim.Domain.Enumerations
+{
+    public class DatabaseTypes : Enumeration
+    {
+        public static readonly DatabaseTypes Memory = new(0, nameof(Memory));
+        public static readonly DatabaseTypes MySQL = new(1, nameof(MySQL));
+
+        public DatabaseTypes(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
